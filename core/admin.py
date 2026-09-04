3a1c1bfa3e7f78bd170b6b68bd453d24a5bfbdd4
Fabiano Chavier dos Santos <chavierwebmidia2026@gmail.com>
2026-09-04 16:49:13 -0300

    Atualização de banco de dados admin django
    ok

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,71 @@
 from django.contrib import admin
+from .models import Brand, Category, Product
 
-# Register your models here.
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'name',
+        'is_active',
+        'description',
+        
+    )
+
+    list_filter = (
+        'is_active',
+      
+    )
+
+    search_fields = (
+        'name',
+        'description',
+    )
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'name',
+        'is_active',
+        'description',
+        
+    )
+
+    list_filter = (
+        'is_active',
+        
+    )
+
+    search_fields = (
+        'name',
+        'description',
+    )
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+
+    list_display = (
+        'name',
+        'brand',
+        'category',
+        'is_active',
+        'price',
+        'description',
+        
+    )
+
+    list_filter = (
+        'is_active',
+        'brand',
+        'category',
+       
+    )
+
+    search_fields = (
+        'name',
+        'description',
+       
+    )
